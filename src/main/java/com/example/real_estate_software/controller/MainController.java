ee00f74a953751f3eb4c7f8863f9c1b547d6607d
StevenHujbert <steven_hujbert@hotmail.com>
2024-09-16 20:57:11 +1000

    Added edit and delete account

--- src/main/java/com/example/real_estate_software/controller/MainController.java
+++ src/main/java/com/example/real_estate_software/controller/MainController.java
@@ -7,18 +7,50 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.text.Text;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import java.io.IOException;
 import java.util.List;
 
 public class MainController {
     @FXML
+    private TextField firstNameField;
+    @FXML
+    private TextField lastNameField;
+    @FXML
+    private TextField emailField;
+    @FXML
+    private TextField passwordField;
+    @FXML
+    private Button deleteButton;
+    @FXML
     private Button logOutButton;
     private OwnerDAO ownerDAO;
 
     public MainController() {
         ownerDAO = new OwnerDAO();
+    }
+
+    @FXML
+    protected void onEditClick() {
+        if(!emptyFields()) {
+            Owner currentOwner = ownerDAO.getOwner(true);
+            currentOwner.setFirstName(firstNameField.getText());
+            currentOwner.setLastName(lastNameField.getText());
+            currentOwner.setEmail(emailField.getText());
+            currentOwner.setPassword(passwordField.getText());
+            ownerDAO.updateOwner(currentOwner);
+        }
+    }
+
+    @FXML
+    protected void onDeleteClick() throws IOException {
+        Owner currentOwner = ownerDAO.getOwner(true);
+        ownerDAO.deleteOwner(currentOwner);
+        Stage stage = (Stage) deleteButton.getScene().getWindow();
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("LoginPage.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), HelloApplication.WIDTH, HelloApplication.HEIGHT);
+        stage.setScene(scene);
     }
 
     @FXML
@@ -34,5 +66,16 @@ public class MainController {
                 stage.setScene(scene);
             }
         }
+    }
+
+    private boolean emptyFields() {
+        boolean emptyFirstName = firstNameField.getText().trim().isEmpty();
+        boolean emptyLastName = lastNameField.getText().trim().isEmpty();
+        boolean emptyEmail = emailField.getText().trim().isEmpty();
+        boolean emptyPassword = passwordField.getText().trim().isEmpty();
+        if(emptyFirstName || emptyLastName || emptyEmail || emptyPassword) {
+            return true;
+        }
+        return false;
     }
 }
