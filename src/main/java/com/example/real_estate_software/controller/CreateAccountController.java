eb844c3483ed8c523f5df7724d99297b7b3b7677
StevenHujbert <steven_hujbert@hotmail.com>
2024-09-16 15:02:18 +1000

    Added signIn and signUp

--- src/main/java/com/example/real_estate_software/controller/CreateAccountController.java
+++ src/main/java/com/example/real_estate_software/controller/CreateAccountController.java
@@ -2,24 +2,48 @@ package com.example.real_estate_software.controller;
 
 
 import com.example.real_estate_software.HelloApplication;
+import com.example.real_estate_software.model.Owner;
+import com.example.real_estate_software.model.OwnerDAO;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import java.io.IOException;
 
 public class CreateAccountController {
     @FXML
-    public Button createAccountButton;
+    private Button createAccountButton;
+    @FXML
+    private TextField firstNameField;
+    @FXML
+    private TextField lastNameField;
+    @FXML
+    private TextField emailField;
+    @FXML
+    private TextField passwordField;
+    private OwnerDAO ownerDAO;
 
+    public CreateAccountController() {
+        ownerDAO = new OwnerDAO();
+    }
 
     @FXML
     protected void onCreateAccountClick() throws IOException {
+        createAccount();
         Stage stage = (Stage) createAccountButton.getScene().getWindow();
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("main-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), HelloApplication.WIDTH, HelloApplication.HEIGHT);
         stage.setScene(scene);
+    }
+
+    private void createAccount() {
+        String firstName = firstNameField.getText();
+        String lastName = lastNameField.getText();
+        String email = emailField.getText();
+        String password = passwordField.getText();
+        ownerDAO.addOwner(new Owner(firstName, lastName, email, password));
     }
 }
 
