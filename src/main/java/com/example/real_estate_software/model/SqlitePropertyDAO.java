c77fcd80ca10f29c4014368ea8cdff82269c6a02
MEGATr0nnn <harrison.mega@outlook.com>
2024-09-17 10:06:09 +1000

    Inserted create property table

--- src/main/java/com/example/real_estate_software/model/SqlitePropertyDAO.java
+++ src/main/java/com/example/real_estate_software/model/SqlitePropertyDAO.java
@@ -2,6 +2,7 @@ package com.example.real_estate_software.model;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class SqlitePropertyDAO {
     private Connection connection;
@@ -10,19 +11,37 @@ public class SqlitePropertyDAO {
         connection = SqliteConnection.getInstance();
     }
 
-    public void create_Table() {
-        //temp
+    public void create_Table_Property() {
+        try{
+            //this still needs to be edited so that the tenant primary key is pulled
+            Statement statement = connection.createStatement();
+            String query = "CREATE TABLE IF NOT EXISTS properties ("
+                    + "property_Id INTEGER PRIMARY KEY AUTOINCREMENT,"
+                    + "address VARCHAR NOT NULL"
+                    + "tenanted BOOLEAN NOT NULL"
+                    + "number_Tenants INTEGER NOT NULL"
+                    + "num_Beds INTEGER NOT NULL"
+                    + "num_Baths INTEGER NOT NULL"
+                    + "num_Carspaces INTEGER NOT NULL"
+                    + "weekly_Rent VARCHAR NOT NULL"
+                    + "weekly_Utilities VARCHAR"
+                    + ")";
+            statement.execute(query);
+        }
+        catch (Exception ex){
+            System.err.println(ex);
+        }
     }
 
-    public void insert() {
+    public void insert_New_Property() {
         //temp will fill out
     }
 
-    public void update() {
+    public void update_Property() {
         //temp
     }
 
-    public void delete() {
+    public void delete_Property() {
         //temp
     }
 
