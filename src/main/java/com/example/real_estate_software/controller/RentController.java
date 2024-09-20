d3be8fc8f1c3702d0ee6f8444dc3d8d4b13df30d
Shanthu Jayamaha <shanthu.jayamaha001@gmail.com>
2024-09-20 17:11:24 +1000

    Added Rent Page and functionality. Need to now find a way to get the number of tenants from the Property Dashboard and only create that number of Renters on the Rent page

---
+++ src/main/java/com/example/real_estate_software/controller/RentController.java
@@ -0,0 +1,20 @@
+package com.example.real_estate_software.controller;
+
+import com.example.real_estate_software.HelloApplication;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class RentController {
+    public Button backButton;
+
+    public void onBackClick() throws IOException {
+        Stage stage = (Stage) backButton.getScene().getWindow();
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("PropertyDashboard.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setScene(scene);
+    }
+}
