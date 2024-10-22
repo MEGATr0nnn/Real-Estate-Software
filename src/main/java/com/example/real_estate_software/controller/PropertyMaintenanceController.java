e901ea6f092e77ab194d35670d50ed3d12f36246
Shanthu Jayamaha <shanthu.jayamaha001@gmail.com>
2024-10-22 12:34:47 +1000

    Made significant changes to design aspects, removed icons from all pages as they are now out of scope (Won't haves) due to time restraints. Additionally added base functionality for

---
+++ src/main/java/com/example/real_estate_software/controller/PropertyMaintenanceController.java
@@ -0,0 +1,6 @@
+package com.example.real_estate_software.controller;
+public class PropertyMaintenanceController {
+    public void onAddFiles() {
+        //NEED TO IMPLEMENT FOR FILE UPLOAD
+    }
+}
