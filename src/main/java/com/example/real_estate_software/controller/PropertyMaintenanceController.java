31009b0229e6fc00048b5dccae866ef21d29c119
Shanthu Jayamaha <shanthu.jayamaha001@gmail.com>
2024-10-27 14:27:46 +1000

    Added full functionality for uploading PDF and docx to repository, created a new directory called Maintenance Files which stores all the uploaded documents. Only allows for PDF and Word Docs, therefore no images etc. Updated test suite too

--- src/main/java/com/example/real_estate_software/controller/PropertyMaintenanceController.java
+++ src/main/java/com/example/real_estate_software/controller/PropertyMaintenanceController.java
@@ -5,9 +5,18 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+
+import java.io.File;
 import java.io.IOException;
 import java.util.Objects;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 /**
  * Class that is used for the Property Maintenance Page [WORK-IN-PROGRESS]
@@ -15,6 +24,8 @@ import java.util.Objects;
 public class PropertyMaintenanceController extends AbstractController {
     @FXML
     private Button backButton;
+    @FXML
+    private Button addFileButton;
 
     public PropertyMaintenanceController() {
         super();
@@ -36,5 +47,30 @@ public class PropertyMaintenanceController extends AbstractController {
         scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/com/example/real_estate_software/stylesheet.css")).toExternalForm());
         stage.setScene(scene);
         stage.show();
+    }
+
+    /**
+     * Button action to add an image associated with the tenant
+     */
+    @FXML
+    protected void onAddFileClick() throws IOException {
+        Stage stage = (Stage) addFileButton.getScene().getWindow();
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Upload File");
+
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("PDF and Word Documents", "*.pdf", "*.docx"));
+
+        File filePath = fileChooser.showOpenDialog(stage);
+
+        if (filePath != null){
+                Path maintenanceFormsFolder = Paths.get("src/main/resources/com/example/real_estate_software/Maintenance Files");
+
+                if (!Files.exists(maintenanceFormsFolder)) {
+                    Files.createDirectories(maintenanceFormsFolder);
+                }
+                Path targetPath = maintenanceFormsFolder.resolve(filePath.getName());
+
+                Files.copy(filePath.toPath(), targetPath);
+        }
     }
 }
